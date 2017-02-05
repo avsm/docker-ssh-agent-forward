d615ba5d2893b592694524156cede98b75b96a15
Martin Honermeyer <maze@strahlungsfrei.de>
2017-02-05 14:14:04 +0100

    Rename LOCAL_* variables to HOST_* (because that's what it is now)

--- pinata-ssh-forward.sh
+++ pinata-ssh-forward.sh
@@ -3,7 +3,7 @@ set -e
 
 IMAGE_NAME=pinata-sshd
 CONTAINER_NAME=pinata-sshd
-LOCAL_PORT=2244
+HOST_PORT=2244
 AUTHORIZED_KEYS=$(ssh-add -L | base64 -w0)
 KNOWN_HOSTS_FILE=$(mktemp)
 
@@ -13,17 +13,17 @@ docker rm -f ${CONTAINER_NAME} >/dev/null 2>&1 || true
 
 docker run --name ${CONTAINER_NAME} \
   -e AUTHORIZED_KEYS="${AUTHORIZED_KEYS}" \
-  -d -p ${LOCAL_PORT}:22 ${IMAGE_NAME} > /dev/null
+  -d -p ${HOST_PORT}:22 ${IMAGE_NAME} > /dev/null
 
 if [ "${DOCKER_HOST}" ]; then
-  IP=$(echo $DOCKER_HOST | awk -F '//' '{print $2}' | awk -F ':' '{print $1}')
+  HOST_IP=$(echo $DOCKER_HOST | awk -F '//' '{print $2}' | awk -F ':' '{print $1}')
 else
-  IP=127.0.0.1
+  HOST_IP=127.0.0.1
 fi
-ssh-keyscan -p ${LOCAL_PORT} ${IP} > ${KNOWN_HOSTS_FILE} 2>/dev/null
+ssh-keyscan -p ${HOST_PORT} ${HOST_IP} > ${KNOWN_HOSTS_FILE} 2>/dev/null
 
 ssh -f -o "UserKnownHostsFile=${KNOWN_HOSTS_FILE}" \
-  -A -p ${LOCAL_PORT} root@${IP} \
+  -A -p ${HOST_PORT} root@${HOST_IP} \
   /root/ssh-forward-agent.sh
 
 echo 'Agent forwarding successfully started.'
